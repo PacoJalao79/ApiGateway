b669d0bcc2c18510cba3c4f30db388f90fc71013
Daniel <dana.gonzalezv@duocuc.cl>
2025-05-28 20:01:30 -0400

    cambios del profe

--- src/main/java/com/gateway/AuthApiSpringBootApplication.java
+++ src/main/java/com/gateway/AuthApiSpringBootApplication.java
@@ -1,13 +1,13 @@
-package com.example.ApiGateway;
+package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class AuthApiSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(AuthApiSpringBootApplication.class, args);
 	}
 
 }
--- src/test/java/com/personas/personas_api_spring_boot/PersonasApiSpringBootApplicationTests.java
+++ src/test/java/com/personas/personas_api_spring_boot/PersonasApiSpringBootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ApiGateway;
+package com.personas.personas_api_spring_boot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class PersonasApiSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
